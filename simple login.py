610c9eeaf65b4f5a6f596268924ebb4bc3ae1f8f
ALi Shhoury <alishhoury78@gmail.com>
2025-02-12 21:11:01 +0200

    simple login

---
+++ simple login.py
@@ -0,0 +1,6 @@
+username = str(input("Please input username:"))
+password = str(input("Please input password:"))
+if username == "admin" and password == "1234" :
+    print("Access Granted")
+else:
+    print("Access Denied")
